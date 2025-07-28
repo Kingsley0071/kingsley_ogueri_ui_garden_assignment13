56f0cf2395b0c7057b347b0f51b7fba6194aab95
Kingsley0071 <kogueri@rrc.ca>
2025-07-27 20:20:22 -0500

    Fix remaining Prettier and ESLint issues

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import logo from './logo.svg';
 // import './App.css';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
 function App() {
   return (
     <div className="App">
